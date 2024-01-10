cff081d37a47f79d34a991d5b545e3505c261356
Mehrajmithu <67754468+Mehrajmithu@users.noreply.github.com>
2024-01-10 12:36:49 +0600

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,11 @@
+
+/* This problem  dooesn't have any input so you just need to print Hello World! as output only*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+   cout<<"Hello World!\n";
+}
