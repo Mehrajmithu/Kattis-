91c5cb3aa237dd58776ab61ab62818d0767bdb40
Mehrajmithu <67754468+Mehrajmithu@users.noreply.github.com>
2024-01-10 12:42:26 +0600

    Add files via upload

---
+++ Stuck In A Time Loop.cpp
@@ -0,0 +1,17 @@
+
+/* IN this problem you need to print Abracadabra n times followed by a sequence of i,Where i is Index */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+vector<ll>ar[2000];
+ll n,m;
+
+int main()
+{
+   ll n;
+   cin>>n;
+   for(int i=1;i<=n;i++)
+   {
+       cout<<i<<" Abracadabra\n";
+   }
+}
